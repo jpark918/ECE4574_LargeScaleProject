494b02d40fa9f334d97f395e97c5edcf1569bb4f
jpark918 <77861847+jpark918@users.noreply.github.com>
2022-12-07 19:00:21 -0500

    Create mainwindow.h

---
+++ For Final Submission/mainwindow.h
@@ -0,0 +1,37 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtCharts>
+#include <QChartView>
+#include <QLineSeries>
+
+
+/*
+ *  Mainwindow.h Park
+ *
+ *  (OUTDATED)
+ *  This file is the header file for mainwindow.cpp
+ *  back when c++ was the language that was going to be used.
+ */
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_lineEdit_returnPressed();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
